05a5092b6a500a966a086532c7a9c03058fe532c
Leslie Lopez <leslie2515lopez@gmail.com>
2022-12-07 21:43:58 -0600

    Installed redux-logger

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 import "./index.css";
 import App from "./containers/App";
 import "tachyons";
 import { searchRobots } from "./reducers";
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 const root = createRoot(document.getElementById("root"));
 root.render(
